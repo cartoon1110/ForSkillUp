54090057e79b9e2526564f3305eb1b15279b5842
sh.yun <sh.yun@navercorp.com>
2016-12-01 18:04:02 +0900

    vs2010 test

---
+++ test/test/test.cpp
@@ -0,0 +1,13 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
+
