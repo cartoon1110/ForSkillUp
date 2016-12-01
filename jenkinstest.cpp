5fb7d9953ae14abb610b78f3fedb7b14bd0d3871
Yun Sung Hyeon <cartoon1110@hanmail.net>
2016-12-01 17:40:58 +0900

    test upload

---
+++ jenkinstest.cpp
@@ -0,0 +1,13 @@
+// jenkinstest.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello world" << endl;
+	return 0;
+}
+
